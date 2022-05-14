3b1b0c75e4d205bc703521682f8f7fee97b112f7
Bruce <lsq991@gmail.com>
2022-05-14 20:39:28 +1000

    wip

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,6 @@
+
+import edu.princeton.cs.algs4.StdOut;
+
 /* *****************************************************************************
  *  Name:              Ada Lovelace
  *  Coursera User ID:  123456
@@ -7,5 +10,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        StdOut.println("Hello, World 2");
     }
 }
