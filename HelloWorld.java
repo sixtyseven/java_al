02783581d2f267cf68ab527bb29eba6bee06282b
bli <bruce.l@covergenius.com>
2022-11-08 00:54:52 +1100

    Init assignment_3 Point.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,11 @@ import edu.princeton.cs.algs4.StdOut;
  *  Name:              Ada Lovelace
  *  Coursera User ID:  123456
  *  Last modified:     October 16, 1842
- **************************************************************************** */
+ **x************************************************************************** */
 
 public class HelloWorld {
     public static void main(String[] args) {
-          System.out.println(" Hello, World");
+        System.out.println(" Hello, World");
         StdOut.println("Hello, World 2");
     }
 }
